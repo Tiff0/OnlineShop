8ce57923c3ca3c7f3c48ade12069e05c86e7f1a9
Zhenya <zTiff0@gmail.com>
2018-03-17 18:56:07 +0300

    Simple local data implemented

---
+++ OnlineShop/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using OnlineShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List() => View(repository.Products);
+
+    }
+}
---
+++ OnlineShop/Models/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace OnlineShop.Models
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+    }
+}
