b9ca8868a97ff67adbd1540f53ae494430444527
Zhenya <zTiff0@gmail.com>
2018-04-14 15:42:09 +0300

    Routing fix

--- OnlineShop/Startup.cs
+++ OnlineShop/Startup.cs
@@ -32,6 +32,14 @@ namespace OnlineShop
             app.UseStaticFiles();
             app.UseMvc(routes => {
                 routes.MapRoute(
+                    name: "pagination", 
+                    template: "Products/Page{productPage}", 
+                    defaults: new {
+                        Controller = "Product",
+                        action = "List"
+                    });
+
+                routes.MapRoute(
                     name: "default", 
                     template: "{controller=Product}/{action=List}/{id?}");
             });
