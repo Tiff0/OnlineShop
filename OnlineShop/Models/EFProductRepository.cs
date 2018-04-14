79499bf7dc23974993b9a7c2e7ebaa285ca2a494
Zhenya <zTiff0@gmail.com>
2018-04-14 10:39:33 +0300

    Added Entity Framework support + made Product migrations

--- OnlineShop/Models/EFProductRepository.cs
+++ OnlineShop/Models/EFProductRepository.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace SportsStore.Models
+namespace OnlineShop.Models
 {
     public class EFProductRepository : IProductRepository
     {
--- OnlineShop/Startup.cs
+++ OnlineShop/Startup.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OnlineShop.Models;
 
@@ -12,12 +9,19 @@ namespace OnlineShop
 {
     public class Startup
     {
+        public Startup (IConfiguration configuration) => Configuration = configuration; 
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration["Data:OnlineStoreProducts:ConnectionString"]));
+            services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
-            services.AddTransient<IProductRepository, FakeProductRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,6 +35,7 @@ namespace OnlineShop
                     name: "default", 
                     template: "{controller=Product}/{action=List}/{id?}");
             });
+            SeedData.EnsurePopulated(app);
         }
     }
 }
