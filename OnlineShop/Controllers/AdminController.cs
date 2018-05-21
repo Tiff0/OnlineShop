89a2918e2cda59b44f6394081bf696a5cfd8fe8a
Zhenya <zTiff0@gmail.com>
2018-05-21 14:08:28 +0300

    added edit fuctions for admins

---
+++ OnlineShop/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Models;
+using System.Linq;
+
+namespace OnlineShop.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository productRepository;
+
+        public AdminController(IProductRepository repo)
+        {
+            productRepository = repo;
+        }
+
+        public ViewResult Index() => View(productRepository.Products);
+
+        public ViewResult Edit(int productID) =>
+            View(productRepository.Products.FirstOrDefault(o => o.ProductID == productID));
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                productRepository.SaveProduct(product);
+                TempData["message"] = $"(product.Name) has been saved";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // there is smth wrong with data
+                return View(product);
+            }
+        }
+    }
+}
--- OnlineShop/Models/IProductRepository.cs
+++ OnlineShop/Models/IProductRepository.cs
@@ -5,5 +5,7 @@ namespace OnlineShop.Models
     public interface IProductRepository
     {
         IQueryable<Product> Products { get; }
+
+        void SaveProduct(Product product);
     }
 }
